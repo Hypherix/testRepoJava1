ada6c4716a4a40cecd22819fa725e39b010d23dc
Hypherix <118616820+Hypherix@users.noreply.github.com>
2024-04-23 15:55:07 +0200

    Added print to main file

--- src/main/java/myjavaproject/myjavaproject1/MyJavaProject1.java
+++ src/main/java/myjavaproject/myjavaproject1/MyJavaProject1.java
@@ -13,5 +13,6 @@ public class MyJavaProject1 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("What's up?");
+        System.out.println("Nothing, you?");
     }
 }
