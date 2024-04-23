07d7b8e2dbb00d797eb3af6e5dfbecc9bc855d58
Hypherix <118616820+Hypherix@users.noreply.github.com>
2024-04-23 11:18:45 +0200

    Added project files to repository

---
+++ src/main/java/myjavaproject/myjavaproject1/MyJavaProject1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package myjavaproject.myjavaproject1;
+
+/**
+ *
+ * @author isakc
+ */
+public class MyJavaProject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
